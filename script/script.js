34bfd6b98f66f1388c32356facc5f607942ce821
Marcus Carvalho <marcusferreiracarvalho@gmail.com>
2023-11-22 21:19:09 -0300

    Adicionando os arquivos da página de cadastro e o script

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function abrirCadastro(){
+    document.getElementById('telaCadastro').style.display = "block"
+}
+
+function fecharCadastro(){
+    document.getElementById('telaCadastro').style.display = "none"
+}
