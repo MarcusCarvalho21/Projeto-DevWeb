df684c7e98bed7114e707544fc571eb6740d787c
Marcus Carvalho <marcusferreiracarvalho@gmail.com>
2023-11-25 16:29:12 -0300

    Adicionando um botão de modo escuroe um botão de menu para mobile

--- script/script.js
+++ script/script.js
@@ -1,3 +1,6 @@
+const temaEscuro = document.getElementById('temaEscuro');
+const navbar = document.getElementById('BarraNavegacao');
+
 function abrirCadastro(){
     document.getElementById('telaCadastro').style.display = "block"
 }
@@ -5,3 +8,9 @@ function abrirCadastro(){
 function fecharCadastro(){
     document.getElementById('telaCadastro').style.display = "none"
 }
+
+temaEscuro.addEventListener('change', ()=>{
+
+    document.body.classList.toggle('tema-escuro');
+    navbar.classList.toggle('navbar-dark')
+})
