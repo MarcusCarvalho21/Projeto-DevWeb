72112a5e93dd36ce87e2f29a51d2eb7109f195ee
Marcus Carvalho <marcusferreiracarvalho@gmail.com>
2023-12-03 16:50:56 -0300

    Adicionando função atualizar tarefa

--- setup/urls.py
+++ setup/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tarefas.views import home, verTarefa
+from tarefas.views import home, verTarefa, editTarefa
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('tarefa/<str:encoded_id>/', verTarefa, name='verTarefa'),
+    path('edit/<int:id>', editTarefa, name='editTarefa'),
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -15,6 +15,19 @@ def home(request):
     tarefas = Tarefas.objects.all().order_by('termino')
     return render(request, "tarefas/home.html", {"tarefas": tarefas})
 
+def editTarefa(request, id):
+    tarefa = get_object_or_404(Tarefas, pk=id)
+    form = TarefaForm(instance=tarefa)
+    if request.method == 'POST':
+        form = TarefaForm(request.POST, instance=tarefa)
+        if form.is_valid():
+            tarefa.save()
+            return redirect('/')
+        else:
+            return render(request, 'tarefas/editaTarefa.html', {'form': form, 'tarefa': tarefa})
+    else:
+        return render(request, 'tarefas/editaTarefa.html', {'form': form, 'tarefa': tarefa})
+
 def verTarefa(request, encoded_id):
     decoded_id = base64.b64decode(encoded_id).decode('utf-8')
     tarefa = get_object_or_404(Tarefas, pk=decoded_id)
